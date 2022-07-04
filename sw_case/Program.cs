13a0dc3f31c8389415e2051af3c0469e64c02c2e
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-07-04 15:03:00 +0300

    Add files via upload

---
+++ sw_case/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace proje_ismi
+{
+    class Program
+    {
+        enum Islemler
+        {
+            Toplama = 1,
+            Cikarma = 2,
+            Carpma = 3,
+            Bolme = 4
+        }
+        static void Main(string[] args)
+            {
+                System.Console.Write("1.Sayıyı Giriniz: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                System.Console.Write("1.Sayıyı Giriniz: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Islemler secim = (Islemler)(new Random().Next(1,4));
+
+
+                switch (secim)
+                {
+                    case Islemler.Toplama:
+                        System.Console.WriteLine($"{a} + {b} = {a + b}  ");
+                        break;
+
+                    case Islemler.Cikarma:
+                        System.Console.WriteLine($"{a} - {b} = {a - b}  ");
+                        break;
+
+                    case Islemler.Carpma:
+                        System.Console.WriteLine($"{a} * {b} = {a * b}  ");
+                        break;
+
+                    case Islemler.Bolme:
+                        System.Console.WriteLine($"{a} / {b} = {a / b}  ");
+                        break;
+
+                    default:
+                        System.Console.WriteLine("\aGeçersiz İşlem!");
+                        break;
+                }
+                Console.ReadKey();
+            }
+     }
+}
+
