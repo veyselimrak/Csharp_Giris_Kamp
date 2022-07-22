610691bd6a4cbe8d2d36daa5b33fa476e321e4cb
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-07-22 20:43:06 +0300

    Add files via upload

---
+++ Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                //Tanımlama
+                var sayilar = new List<int>();
+
+                //Ekleme
+                sayilar.Add(25);
+                sayilar.Add(35);
+                sayilar.Add(45);
+                sayilar.Insert(3,100);
+
+                //Listeye Dizi Ekleme
+                int[] numbers = new int[]{10,20,30};
+
+                foreach (var number in numbers)
+                {
+                    sayilar.Add(number);
+                }
+                
+                foreach (var sayi in sayilar)
+                {
+                    System.Console.Write($" {sayi,-5}");
+                }
+
+
+                //Çıkarma
+
+                sayilar.Remove(35);
+                sayilar.RemoveAt(1);
+                sayilar.RemoveRange(1,2);
+
+
+                foreach (var sayi in sayilar)
+                {
+                    System.Console.WriteLine();
+                    System.Console.Write($" {sayi,-5}");
+                }
+
+                Console.ReadKey();
+
+            }
+    }
+}
