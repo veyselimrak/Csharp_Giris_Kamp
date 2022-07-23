71278b7f7bfbd79af4fa67047e461ca3b6b2b64e
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-07-23 23:03:37 +0300

    Add files via upload

---
+++ metotlar1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                double toplam = seriToplami(5.5, 15.28, 32.19);
+                System.Console.WriteLine($" {toplam,5:0.##}  ");
+
+                Console.ReadKey();
+
+            }
+        
+        private static double seriToplami(params double[] seri)
+        {
+            double toplam = 0;
+            foreach (double sayi in seri)
+            {
+                toplam += sayi;
+            }
+            return toplam;
+        }
+
+    }
+}
