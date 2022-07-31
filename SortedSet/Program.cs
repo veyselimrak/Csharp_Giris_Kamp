7b9d5f50797e9c6b30b74c7c01e366c36c39de12
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-07-31 19:13:45 +0300

    Add files via upload

---
+++ SortedSet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSetIslemler();
+            Console.ReadKey();
+
+        }
+
+        private static void SortedSetIslemler()
+        {
+            //SotedSet Tanımlama
+            var list = new SortedSet<string>();
+
+            //Ekleme - Silme 
+            list.Add("Veysel");
+            if (list.Add("Veysel"))
+            {
+                System.Console.WriteLine("Başarıyla Eklendi.");
+            }
+            else
+            {
+                System.Console.WriteLine("Ekleme işlemi başarısız.");
+            }
+            list.Add("Fatih");
+            list.Add("Mehmet");
+            list.Add("Emre");
+
+
+            list.Remove("Veysel");
+            //Belirtilen koşulları bulunduran değerleri siler.
+            list.RemoveWhere(deger => deger.Contains("e"));
+            list.RemoveWhere(deger => deger.StartsWith("E"));
+
+
+            //Liste üzerinde Dolaşma
+            System.Console.WriteLine("\nİsimler Listesi");
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
